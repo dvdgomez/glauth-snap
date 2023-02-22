f0da118fd0f8c3115de5787affdacdd2a6f2962e
dvdgomez <108831013+dvdgomez@users.noreply.github.com>
2023-02-22 11:55:11 -0800

    Bootstrap GLAuth Snap (#1)
    * initial bootstrap of snap
    
    * use Apache LICENSE
    
    * updated README
    
    * add initial snap tests
    
    * updated tests and snapcraft yaml
    
    * latest snapcraft changes
    
    * can build snap with makefile with right version
    
    * snap with version set
    
    * updated test that passes lint and fmt jobs
    
    * add tox and toml
    
    * add license header to snap yaml
    
    * github ci workflows
    
    * fix release yaml indent
    
    * add network plugs
    
    * add network test
    
    * fix lint
    
    * add snapcraft for testing
    
    * Update snap/snapcraft.yaml
    
    Co-authored-by: Jason Nucciarone <40342202+NucciTheBoss@users.noreply.github.com>
    
    * snapcraft linting and extra tox commands
    
    * update unit tests to use tox and add snapcraft to workflow
    
    * use snapcore/action-publish
    
    * Update snap/snapcraft.yaml
    
    Co-authored-by: Jason Nucciarone <40342202+NucciTheBoss@users.noreply.github.com>
    
    * Update tox.ini
    
    Co-authored-by: Jason Nucciarone <40342202+NucciTheBoss@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Jason Nucciarone <40342202+NucciTheBoss@users.noreply.github.com>

---
+++ tests/unit/test_glauth.py
@@ -0,0 +1,63 @@
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Test minimal snap."""
+
+import logging
+import os
+import pathlib
+import re
+import shlex
+import subprocess
+import time
+import unittest
+
+import requests
+
+logger = logging.getLogger(__name__)
+
+
+class TestSnap(unittest.TestCase):
+    """Unit test GLAuth snap."""
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        """Test class setup."""
+        subprocess.run(shlex.split("tox -e clean"))
+        logger.info("Building snap")
+        # Go up 3 levels from test file
+        os.chdir("/".join(__file__.split("/")[:-3]))
+        subprocess.run(shlex.split("tox -e snap"))
+        # Find generated snap
+        cls.GLAUTH = re.findall(r"(glauth.*?snap)", " ".join(os.listdir()))[0]
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        """Test class teardown."""
+        pathlib.Path("sample-simple.cfg").unlink(missing_ok=True)
+        subprocess.run(shlex.split("tox -e clean"))
+
+    def test_build(self):
+        """Test snap build status."""
+        logger.info(f"Checking for snap {TestSnap.GLAUTH}...")
+        self.assertTrue(pathlib.Path(TestSnap.GLAUTH).exists())
+
+    def test_run(self):
+        """Test snap run status."""
+        logger.info(f"Installing glauth snap {TestSnap.GLAUTH}...")
+        subprocess.run(shlex.split("tox -e install"))
+        logger.info("Finished glauth snap install!")
+        source = subprocess.run(
+            shlex.split("which glauth"), stdout=subprocess.PIPE, text=True
+        ).stdout.strip("\n")
+        self.assertEqual(source, "/snap/bin/glauth")
+
+    def test_run_config(self):
+        """Test snap network status."""
+        logger.info("Testing network status")
+        wait = subprocess.Popen(shlex.split("glauth -c sample-simple.cfg"))
+        time.sleep(5)
+        # Check snap responses
+        response = requests.get("http://localhost:5555")
+        self.assertEqual(response.status_code, 200)
+        wait.kill()
